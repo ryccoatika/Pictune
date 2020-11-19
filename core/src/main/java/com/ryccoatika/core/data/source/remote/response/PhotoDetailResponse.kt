53b5fe12f0433da7d85c8a09b9b83ccbc79a752c
ryccoatika <ryccoatika.ra@gmail.com>
2020-11-19 23:23:39 +0700

    merge the response

--- core/src/main/java/com/ryccoatika/core/data/source/remote/response/PhotoDetailResponse.kt
+++ core/src/main/java/com/ryccoatika/core/data/source/remote/response/PhotoDetailResponse.kt
@@ -24,9 +24,9 @@ data class PhotoDetailResponse(
     @SerializedName("alt_description")
     val altDescription: String? = null,
     @SerializedName("urls")
-    val urls: UrlResponse? = null,
+    val urls: UrlsResponse? = null,
     @SerializedName("links")
-    val links: LinkPhotoResponse? = null,
+    val links: LinksPhotoResponse? = null,
     @SerializedName("likes")
     val likes: Int? = null,
     @SerializedName("liked_by_user")
@@ -40,9 +40,44 @@ data class PhotoDetailResponse(
     @SerializedName("tags")
     val tags: List<TagResponse>? = null,
     @SerializedName("related_collections")
-    val relatedCollections: List<RelatedCollection>? = null,
+    val relatedCollectionResponses: List<RelatedCollectionResponse>? = null,
     @SerializedName("views")
     val views: Int? = null,
     @SerializedName("downloads")
     val downloads: Int? = null
-)
+) {
+    data class ExifResponse(
+        @SerializedName("make")
+        val make: String? = null,
+        @SerializedName("model")
+        val model: String? = null,
+        @SerializedName("exposure_time")
+        val exposureTime: String? = null,
+        @SerializedName("aperture")
+        val aperture: String? = null,
+        @SerializedName("focal_length")
+        val focalLength: String? = null,
+        @SerializedName("iso")
+        val iso: Int? = null
+    )
+
+    data class LocationResponse(
+        @SerializedName("title")
+        val title: String? = null,
+        @SerializedName("name")
+        val name: String? = null,
+        @SerializedName("city")
+        val city: String? = null,
+        @SerializedName("country")
+        val country: String? = null,
+        @SerializedName("position")
+        val position: PositionResponse? = null
+    ) {
+        data class PositionResponse(
+            @SerializedName("latitude")
+            val latitude: Double? = null,
+            @SerializedName("longitude")
+            val longitude: Double? = null
+        )
+    }
+}
