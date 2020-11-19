98937e597d6438d9694e99d68b4019126dbad3a3
ryccoatika <ryccoatika.ra@gmail.com>
2020-11-19 22:01:40 +0700

    create sealed class for bundling the result from api request

---
+++ core/src/main/java/com/ryccoatika/core/data/source/remote/ApiResponse.kt
@@ -0,0 +1,7 @@
+package com.ryccoatika.core.data.source.remote
+
+sealed class ApiResponse<out R> {
+    data class Success<out T>(val data: T): ApiResponse<T>()
+    data class Error(val message: String): ApiResponse<Nothing>()
+    object Empty: ApiResponse<Nothing>()
+}
