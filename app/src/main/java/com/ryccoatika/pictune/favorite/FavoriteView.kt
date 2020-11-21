818fc534adc954bbeb5746de57f0b041dab0b6cb
ryccoatika <ryccoatika.ra@gmail.com>
2020-11-21 12:08:06 +0700

    don't need it

--- app/src/main/java/com/ryccoatika/pictune/favorite/FavoriteView.kt
+++
@@ -1,9 +0,0 @@
-package com.ryccoatika.pictune.favorite
-
-import androidx.lifecycle.LiveData
-
-interface FavoriteView {
-    val state: LiveData<FavoriteViewState>
-    fun getAllFavoritePhotos()
-    fun getUnsplashPhotoFromInternet(id: String)
-}
