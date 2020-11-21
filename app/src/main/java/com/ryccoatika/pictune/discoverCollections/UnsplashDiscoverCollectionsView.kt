74fd4b3457b9923a951f89679f20f6832c54b844
ryccoatika <ryccoatika.ra@gmail.com>
2020-11-21 12:02:48 +0700

    change the file name

--- app/src/main/java/com/ryccoatika/pictune/discoverCollections/UnsplashDiscoverCollectionsView.kt
+++
@@ -1,9 +0,0 @@
-package com.ryccoatika.pictune.discoverCollections
-
-import androidx.lifecycle.LiveData
-
-interface UnsplashDiscoverCollectionsView {
-    val state: LiveData<UnsplashDiscoverCollectionsViewState>
-    fun discoverCollections(filter: String)
-    fun loadMoreCollections(page: Int, filter: String)
-}
